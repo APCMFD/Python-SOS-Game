5ca655076a0a25d1abab64c6e7e4ed0c5d3ba81f
APCMFD <78195894+APCMFD@users.noreply.github.com>
2022-09-10 11:22:59 -0500

    Update sprint0.py

--- sprint0.py
+++ sprint0.py
@@ -1,35 +1,19 @@
 from tkinter import *
-# widgets = GUI elements: buttons, textboxes, labels, images
-# windows = serves as a container to hold or contain these widgets
-
-
-class Player():
-    def __init__(self, player_num):
-        self.player_num = player_num
-        self.turn = False
-
-    def get_turn(self):
-        return self.turn
-
-    def set_turn(self, turn_status):
-        self.turn = turn_status
-
 
 class SOS_GAME_BOARD():
 
-    NUM_ROWS = 6
-    NUM_COLS = 6
-
     board = []
-
+    ROWS = 6
+    COLS = 6
+    
     def __init__(self):
         self.create_board()
 
-    def create_board(self):
-        for row in range(self.NUM_ROWS):
+    def create_board(self): # makes a board of empty cells
+        for row in range(self.ROWS):
             self.board.append([])
-            for col in range(self.NUM_COLS):
-                self.board[row].append(0)  # append empty cell
+            for col in range(self.COLS):
+                self.board[row].append(0)  
 
     def set_cell_value(self, row, col, value):
         self.board[row][col] = value
@@ -43,30 +27,29 @@ class SOS_GAME_GUI():
     WINDOW_HEIGHT = 1200
     WINDOW = Tk()
     WINDOW.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
-    WINDOW.title("Morgan's SOS Game")
-    BUTTON_HEIGHT = 3
-    BUTTON_WIDTH = 4
-    RED_PLAYER_OPTION =  StringVar()
+    WINDOW.title("Adam's SOS Game")
+    PLAYER_OPTION =  StringVar()
+    BUTTON_HEIGHT = 4
+    BUTTON_WIDTH = 8
 
     def __init__(self):
         self.gameboard = SOS_GAME_BOARD()
 
     def start(self):
         self.create_GUI_board()
-        #self.create_player_buttons()
-        self.WINDOW.mainloop() # place window on computer screen, listen for events
+        self.WINDOW.mainloop() # places window on the computer screen
 
     def create_GUI_board(self):
-        for r in range(self.gameboard.NUM_ROWS):
-            for c in range(self.gameboard.NUM_COLS):
+        for r in range(self.gameboard.ROWS):
+            for c in range(self.gameboard.COLS):
                 self.gameboard.board[r][c] = Button(self.WINDOW, bg="SystemButtonFace", height=self.BUTTON_HEIGHT, width=self.BUTTON_WIDTH)
                 self.gameboard.board[r][c].grid(row=r, column=c)
                 
-        red_player_label = Label(self.WINDOW, text='RED PLAYER')
-        red_player_label.grid(row=2, column=9)
+        player_text = Label(self.WINDOW, text='PLAYER 1')
+        player_text.grid(row=1, column=7)
 
-        red_player_S_button = Radiobutton(self.WINDOW, text='S', variable= self.RED_PLAYER_OPTION, value='S').grid(row=3, column=9)
-        c1 = Checkbutton(self.WINDOW, text='O', variable= self.RED_PLAYER_OPTION, onvalue=1, offvalue=0).grid(row=4, column=9)
+        S_button = Radiobutton(self.WINDOW, text='S', variable= self.PLAYER_OPTION, value='S').grid(row=2, column=7)
+        O_checkbox = Checkbutton(self.WINDOW, text='O', variable= self.PLAYER_OPTION, onvalue=1, offvalue=0).grid(row=3, column=7)
 
 
 
